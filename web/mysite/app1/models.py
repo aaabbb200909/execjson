7397d86dd046a37580d8e89c2bbd3925ef020cbd
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-03-30 22:01:06 +0900

    set primary key

--- web/mysite/app1/models.py
+++ web/mysite/app1/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class user_id_jsons(models.Model):
+    user = models.CharField(max_length=30, primary_key=True) # user1, user2, ...
+    saveid = models.CharField(max_length=30, primary_key=True) # 1,2,...
+    json = models.TextField() # json
