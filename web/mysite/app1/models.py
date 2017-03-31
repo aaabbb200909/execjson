7fc5f20138879c527fdb4d8b56c13ab7076c5666
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-04-01 00:22:36 +0900

    i removed one of two primary keys from model ..

--- web/mysite/app1/models.py
+++ web/mysite/app1/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 # Create your models here.
 class user_id_jsons(models.Model):
+    class Meta:
+        unique_together = (('user', 'saveid'),)
     user = models.CharField(max_length=30, primary_key=True) # user1, user2, ...
-    saveid = models.CharField(max_length=30, primary_key=True) # 1,2,...
+    saveid = models.CharField(max_length=30) # 1,2,...
     json = models.TextField() # json
