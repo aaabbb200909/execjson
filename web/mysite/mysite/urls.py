b01aeb329065d8abb62f3fd194fba5184c9ccd36
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-04-28 00:15:45 +0900

    i added settings for django-debug-toolbar

--- web/mysite/mysite/urls.py
+++ web/mysite/mysite/urls.py
@@ -20,3 +20,13 @@ urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^app1/', include('app1.urls')),
 ]
+
+
+from django.conf import settings
+from django.conf.urls import include, url
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
