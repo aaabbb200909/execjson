4ec3186a58ed5ad119228c24f170461ff06bf242
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-05-25 07:00:08 +0900

    set DEBUG=False for successful docker build

--- web/mysite/mysite/settings.py
+++ web/mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^2c-h@c6&)clx908%ir7inhj#7)0=y@885k%$tna=rr^xnxle('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
