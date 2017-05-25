05979b25a47b3c07158509650b5bd0037ad51286
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-05-25 21:50:15 +0900

    added redirect to app1

--- web/mysite/mysite/urls.py
+++ web/mysite/mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/app1', permanent=False)),
     url(r'^app1/', include('app1.urls')),
 ]
 
