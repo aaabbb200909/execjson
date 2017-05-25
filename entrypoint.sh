07233fca4cc18e6fbb046751e3f4177022b65600
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-05-25 22:14:33 +0900

    added httpd definition file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+httpd
 cd /opt/execjson/web/mysite/batch
-./do.sh &
-cd /opt/execjson/web/mysite
-./manage.py runserver 0.0.0.0:8000
+./do.sh
+# cd /opt/execjson/web/mysite
+# ./manage.py runserver 0.0.0.0:8000
